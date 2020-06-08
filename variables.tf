8307d50818c505f1cdba9cb83d45ec6987a06c7e
Gustavo Alvarado <galvaradocrue@gmail.com>
2020-06-08 17:26:15 -0500

    updated variables

--- variables.tf
+++ variables.tf
@@ -1,12 +1,18 @@
 ### main settings for project, avoid rewrite variables etc...
-
 variable "region" {
   default = "us-east-2"
 }
-variable "environment" {
+variable "account_id" {
+  description = "Id of the account, needed in some resources"
   default = ""
 }
-
-variable "project" {
-  default = ""
+variable "environment" {
+  default = "dev"
+}
+variable "namespace" {
+  default = "practice"
+  description = "Basically, the name of the project"
+}
+variable "delimiter" {
+  default = "-"
 }
