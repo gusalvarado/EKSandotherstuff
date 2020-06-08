d486d42443def7b77d0770b546aeb889b70b1cda
Gustavo Alvarado <galvaradocrue@gmail.com>
2020-06-08 17:26:56 -0500

    updated vpc

--- vpc.tf
+++ vpc.tf
@@ -2,14 +2,14 @@ data "aws_availability_zones" "available" {
 
 }
 locals {
-  cluster_name = "eks-${var.project}-${var.environment}"
+  cluster_name = "eks-${var.namespace}-${var.environment}"
 }
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   version = "2.6.0"
 
-  name = "vpc-${var.project}-${var.environment}"
+  name = "vpc-${var.namespace}-${var.environment}"
   cidr = "10.0.0.0/16"
   azs = data.aws_availability_zones.available.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
