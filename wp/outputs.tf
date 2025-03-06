8d92ec4ebb0c31cd1dc4fde2847b043f0d530e19
Gustavo Alvarado <gustavo.alvarado@valtira.net>
2025-03-05 22:46:12 -0600

    adding helm

--- wp/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-    value = module.vpc.vpc_id
-}
-output "public_subnets_id" {
-    value = module.vpc.public_subnets
-}
-output "private_subnets_id" {
-    value = module.vpc.private_subnets
-}
